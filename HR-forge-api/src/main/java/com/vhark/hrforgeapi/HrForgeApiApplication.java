4ca23058113f4b6f60d071fca74230ad4856af04
Vladyslav Harkusha <harkushavlad823@gmail.com>
2024-05-03 14:40:59 +0300

    init workspace

---
+++ HR-forge-api/src/main/java/com/vhark/hrforgeapi/HrForgeApiApplication.java
@@ -0,0 +1,12 @@
+package com.vhark.hrforgeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrForgeApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HrForgeApiApplication.class, args);
+  }
+}
---
+++ HR-forge-api/src/test/java/com/vhark/hrforgeapi/HrForgeApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.vhark.hrforgeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrForgeApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
