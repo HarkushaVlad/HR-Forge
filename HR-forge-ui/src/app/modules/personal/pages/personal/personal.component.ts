06a8685be27909dde5345d52b7fc6fbfe64cd97a
Vladyslav Harkusha <harkushavlad823@gmail.com>
2024-05-19 17:46:57 +0300

    add personal page

--- HR-forge-ui/src/app/modules/personal/pages/personal/personal.component.ts
+++ HR-forge-ui/src/app/modules/personal/pages/personal/personal.component.ts
@@ -1,8 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PersonalService } from '../../../../services/services/personal.service';
+import { EmployeeResponse } from '../../../../services/models/employee-response';
+import { PersonalRequest } from '../../../../services/models/personal-request';
 
 @Component({
   selector: 'app-personal',
   templateUrl: './personal.component.html',
   styleUrl: './personal.component.scss',
 })
-export class PersonalComponent {}
+export class PersonalComponent implements OnInit {
+  personalRequest: PersonalRequest = {
+    birthDate: '',
+    email: '',
+    firstName: '',
+    lastName: '',
+  };
+  errorMsg: Array<String> = [];
+
+  personalInformation: EmployeeResponse = {
+    departmentName: 'Can not load department name',
+    positionName: 'Can not load position name',
+    firstName: 'Can not load name',
+    lastName: 'Can not load surname',
+    birthDate: new Date(1990, 1, 1).toString(),
+    email: 'Can not load email',
+    salary: 0,
+    hireDate: new Date(1990, 1, 1).toString(),
+  };
+
+  isLoading: boolean = true;
+  isError: boolean = false;
+
+  fieldName?: string;
+  currentField?: string;
+  fieldValue?: any;
+  isDateField?: boolean;
+  @ViewChild('editModal') editModal: any;
+
+  constructor(private personalService: PersonalService) {}
+
+  ngOnInit(): void {
+    this.personalService.getPersonal().subscribe({
+      next: (res) => {
+        this.personalInformation = res;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.log(err);
+        this.isLoading = false;
+        this.isError = true;
+      },
+    });
+  }
+
+  openEditDialog(fieldName: string, field: string): void {
+    this.errorMsg = [];
+    this.personalRequest = {
+      birthDate: this.personalInformation.birthDate ?? '',
+      email: this.personalInformation.email ?? '',
+      firstName: this.personalInformation.firstName ?? '',
+      lastName: this.personalInformation.lastName ?? '',
+    };
+
+    this.fieldName = fieldName;
+    this.currentField = field;
+    this.isDateField = field === 'birthDate' || field === 'hireDate';
+    const fieldValue =
+      this.personalInformation[field as keyof EmployeeResponse];
+
+    if (this.isDateField && fieldValue) {
+      this.fieldValue = new Date(fieldValue);
+    } else {
+      this.fieldValue = fieldValue;
+    }
+
+    this.editModal.show();
+  }
+
+  save(): void {
+    this.errorMsg = [];
+    this.personalRequest[this.currentField as keyof PersonalRequest] =
+      this.fieldValue;
+
+    this.isLoading = true;
+
+    this.personalService
+      .updatePersonal({ body: this.personalRequest })
+      .subscribe({
+        next: () => {
+          this.personalInformation[this.currentField as keyof PersonalRequest] =
+            this.fieldValue;
+          this.editModal.hide();
+          this.isLoading = false;
+        },
+        error: (err) => {
+          this.isLoading = false;
+          if (err.error.validationErrors) {
+            this.errorMsg = err.error.validationErrors;
+          } else {
+            this.errorMsg.push(err.error.error);
+          }
+        },
+      });
+  }
+}
--- HR-forge-ui/src/app/modules/personal/personal.module.ts
+++ HR-forge-ui/src/app/modules/personal/personal.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { PersonalRoutingModule } from './personal-routing.module';
 import { PersonalComponent } from './pages/personal/personal.component';
-
+import { FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
-  declarations: [
-    PersonalComponent
-  ],
+  declarations: [PersonalComponent],
   imports: [
     CommonModule,
-    PersonalRoutingModule
-  ]
+    PersonalRoutingModule,
+    FormsModule,
+    ModalModule,
+    BsDatepickerModule,
+    ModalModule.forRoot(),
+  ],
 })
-export class PersonalModule { }
+export class PersonalModule {}
