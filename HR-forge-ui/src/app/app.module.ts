bbc1540ff5af947eb3088916de7e1f6084394bcf
Vladyslav Harkusha <harkushavlad823@gmail.com>
2024-05-19 17:46:00 +0300

    add token interceptor

--- HR-forge-ui/src/app/app.module.ts
+++ HR-forge-ui/src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
@@ -17,7 +22,14 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [HttpClient],
+  providers: [
+    HttpClient,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ HR-forge-ui/src/app/services/interceptor/http-token.interceptor.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { TokenService } from '../token/token.service';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class HttpTokenInterceptor implements HttpInterceptor {
+  constructor(
+    private tokenService: TokenService,
+    private router: Router
+  ) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.tokenService.token;
+    let authReq = req;
+
+    if (token) {
+      authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+
+    return next.handle(authReq).pipe(
+      catchError((error) => {
+        if (error.status === 403 || error.status === 401) {
+          this.tokenService.removeToken();
+          this.router.navigate(['/login']);
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
