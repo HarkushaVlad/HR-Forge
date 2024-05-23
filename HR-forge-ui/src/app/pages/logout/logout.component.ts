12812efb501e06049bf050c3954ddca98192494c
Vladyslav Harkusha <harkushavlad823@gmail.com>
2024-05-23 16:29:22 +0300

    add logout page

--- HR-forge-ui/src/app/pages/logout/logout.component.ts
+++ HR-forge-ui/src/app/pages/logout/logout.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { TokenService } from '../../services/token/token.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrl: './logout.component.scss'
+  styleUrl: './logout.component.scss',
 })
 export class LogoutComponent {
+  constructor(
+    private location: Location,
+    private tokenService: TokenService
+  ) {}
 
+  confirmExit() {
+    this.tokenService.removeToken();
+    window.location.reload();
+  }
+
+  cancelExit() {
+    this.location.back();
+  }
 }
